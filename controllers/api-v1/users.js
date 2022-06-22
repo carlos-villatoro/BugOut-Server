202ba1b5362ceba9a43efba641681de1df91867b
weston-bailey <wdbailey87@gmail.com>
2022-06-22 15:29:34 -0700

    adds login

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -53,8 +53,45 @@ router.post('/register', async (req, res) => {
 })
 
 // POST /users/login -- validate login credentials
-router.post('/login', (req, res) => {
-	res.json({ msg: 'login a user' })
+router.post('/login', async (req, res) => {
+	try {
+		// all the data will come in on the req.body
+		// try to find the user in the database
+		const foundUser = await db.User.findOne({
+			email: req.body.email
+		})
+		const noLoginMessage = 'Incorrect email or password.'
+
+		// if the user is not found, return send a status of 400 let the user know login failed
+		if (!foundUser) {
+			console.log('incorrect email', req.body)
+			return res.status(400).json({ msg: noLoginMessage })
+		}
+
+		// check if the supplied password matches the hash in the db
+		const passwordCheck = await bcrypt.compare(req.body.password, foundUser.password)
+		// if they do not match, return and let the user know that login has failed
+		if (!passwordCheck) {
+			console.log('incorrect password', req.body)
+			return res.status(400).json({ msg: noLoginMessage })
+		}
+
+		// create a jwt payload
+		const payload = {
+			name: foundUser.name,
+			email: foundUser.email,
+			id: foundUser.id
+		}
+		// sign the jwt and send it back
+		const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+		res.json({ token })
+	} catch (err) {
+		// don't forget to handle your errors
+		console.warn(err)
+		res.status(500).json({ msg: 'Server room is on fire 🔥' })
+	}
+
 })
 
 // GET /users/auth-locked -- checks users credentials and only send back privlaged information if the user is logged in properly
