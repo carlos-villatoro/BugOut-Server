dd969e50c683c38417db91ea04a259e64b271533
weston-bailey <wdbailey87@gmail.com>
2022-06-22 11:07:01 -0700

    take a break

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-	name: String,
+	name: {
+		type: String,
+		required: true
+	},
 	email: {
 		type: String,
 		required: true
