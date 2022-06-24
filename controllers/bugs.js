e001b474d45d64f563c14ed7ef2bd23923200ea3
Emily-Herndon <emilyvherndon@gmail.com>
2022-06-24 16:49:38 -0700

    added put route

--- controllers/bugs.js
+++ controllers/bugs.js
@@ -3,7 +3,24 @@ const db = require('../models')
 
 //PUT /bugs/:id -- edit a specific bug
 router.put('/:id', async (req, res) => {
-    res.send(' edit a /bugs/:id')
+    // res.send(' edit a /bugs/:id')
+    try {
+        // find bug id from the url params
+        const id = req.params.id
+        // tell it to return the updated comment
+        const options = {new: true}
+        // find specific comment using that id & updating using the req.body
+        const updatedBug = await db.Bug.findByIdAndUpdate(id, req.body, options)
+        // send updated comment back to the user
+        res.json(updatedBug)
+
+        console.log('you have successfully updated the bug')
+    } catch (error) {
+        if(error.name === "ValidationError"){
+            res.status(400).json({msg: error.message})
+        }
+        res.status(500).json({msg: 'server error'})
+    }
 })
 
 //DELETE /bugs/:id -- delete a specific bug
