adca3c8d3b98354b0fde64858bb01dfcad580972
Emily-Herndon <emilyvherndon@gmail.com>
2022-06-24 14:12:36 -0700

    completed successful test of adding user and projects to each other

--- dbTest.js
+++ dbTest.js
@@ -1,12 +1,41 @@
 const db = require('./models')
+const project = require('./models/project')
 
 // testing user CREATE
-db.User.create({
-		name: 'Test boi',
-		email: 'test@boi.com',
-		password: 'testerboi1234'	
-	})
-	.then(user => {
-		console.log('what up test boi!!!', user)
-	})
-	.catch(console.warn)
+// db.User.create({
+// 		name: 'grunt 2',
+// 		email: 'g2@g.com',
+// 		password: '1234',
+// 		role: "member"	
+// 	})
+// 	.then(user => {
+// 		console.log('what up test boi!!!', user)
+// 	})
+// 	.catch(console.warn)
+
+// db.Project.create({
+// 		name: 'Project 2',
+// 		language: 'react',
+// 		description: '2nd project',
+// 		notes: 'hello',
+// 		priority: 4
+// 	})
+// 	.then(project => {
+// 		console.log(project)
+// 	})
+// 	.catch(console.warn)
+const usersProjectsRel = async () => {
+	try {
+		const user = await db.User.findOne({id: "62b6249d74a4224e8bfb3feb"})
+		const project = await db.Project.findOne({id: "62b6262850a78d04a38e42d8"})
+		user.projects.push(project)
+		await user.save()
+		project.users.push(user)
+		await project.save()
+		console.log(user, project)
+		
+	} catch (error) {
+		console.warn(error)
+	}
+}
+usersProjectsRel()
